337c0d978e349c2fda38ead421e177c501fd2373
satoshi hiruta <cosmographia21@gmail.com>
2017-12-17 13:14:31 +0000

    user pages

--- config/app.php
+++ config/app.php
@@ -147,6 +147,7 @@ return [
         App\Providers\RouteServiceProvider::class,
         
         Collective\Html\HtmlServiceProvider::class,
+        Thomaswelton\LaravelGravatar\LaravelGravatarServiceProvider::class,
 
     ],
 
@@ -198,6 +199,8 @@ return [
         
         'Form'      => Collective\Html\FormFacade::class,
         'Html'      => Collective\Html\HtmlFacade::class,
+        
+        'Gravatar'  => Thomaswelton\LaravelGravatar\Facades\Gravatar::class,
 
     ],
 
