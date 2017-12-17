74d56e83c00cf4ae3a4296b47fc3ab1bfb3188e3
satoshi hiruta <cosmographia21@gmail.com>
2017-12-17 12:50:06 +0000

    user login

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -24,6 +24,7 @@ class AuthController extends Controller
     use AuthenticatesAndRegistersUsers, ThrottlesLogins;
 
     protected $redirectTo = '/';
+    Protected $loginPath = '/login';
 
     /**
      * Create a new authentication controller instance.
